0e560c1df00dbbbb33d400216ea9f3a18ddc4ccf
EnzoBEDOS22002844 <enzo.bedos@etu.univ-amu.fr>
2023-05-03 09:36:53 +0200

    Step 0 finished

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,11 @@
 module com.example.project_jfx {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.project_jfx to javafx.fxml;
     exports com.example.project_jfx;
+    exports com.example.partie1;
 }
