0287fc1e779ecbe426b7f7f85ef19a87fd9c7082
EnzoBEDOS22002844 <enzo.bedos@etu.univ-amu.fr>
2023-05-03 08:58:12 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.project_jfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+                        requires org.kordamp.bootstrapfx.core;
+            
+    opens com.example.project_jfx to javafx.fxml;
+    exports com.example.project_jfx;
+}
